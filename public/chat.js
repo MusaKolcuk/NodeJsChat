b749519d53ccccfc7dad2df0f5a2f6795f08dd43
MusaKolcuk <musa_kolcuk_1999@hotmail.com>
2023-03-04 15:43:43 +0300

    Master

--- public/chat.js
+++ public/chat.js
@@ -7,11 +7,18 @@ const output = document.getElementById('output')
 const feedback = document.getElementById('feedback')
 
 submitBtn.addEventListener('click', () => {
-    socket.emit('chat', {       // socket.emit ile sunucuya bir chat olayi gönderilir. //emit!
+    // chat verilerini Local Storage'a kaydet
+    localStorage.setItem('chat', JSON.stringify({
         message: message.value,
         sender: sender.value
-    })
-})
+    }));
+    // socket.emit ile sunucuya bir chat olayi gönderilir.
+    socket.emit('chat', {
+        message: message.value,
+        sender: sender.value
+    });
+});
+
     socket.on('chat', data =>  {
         feedback.innerHTML = ""
         output.innerHTML += '<p><strong>' + data.sender + ': </strong>' + data.message + '</p>'
@@ -27,4 +34,13 @@ submitBtn.addEventListener('click', () => {
     socket.on("typing",data => {
         feedback.innerHTML = '<p>' + data + ' yaziyor...</p>'
     })
+
+    // sayfa yüklendiğinde Local Storage'dan chat verilerini al
+window.onload = () => {
+    const chatData = JSON.parse(localStorage.getItem('chat'));
+    if (chatData) {
+        sender.value = chatData.sender;
+        message.value = chatData.message;
+    }
+};
 
